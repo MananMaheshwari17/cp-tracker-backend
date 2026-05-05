4bf4ff3fb84dd713aa0e310e8103fa1237bb2c85
MananMaheshwari17 <mananmaheshwariji@gmail.com>
2026-05-05 22:20:56 +0530

    Add dynamic route for user handle

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/hello", (req,res) =>{
     res.send("Hello World!")
 })
 
+app.get("/user/:handle", (req,res) =>{
+    const handle = req.params.handle;
+    res.send(`your requested data for ${handle}`)
+})
+
 app.listen(3000, () =>{
     console.log("server running on port 3000")
 })
